d58dc4ce8f0e7c1379b30b0b6dc2adb3bacf74d2
Ilya Sokolov <developer.sokolov@gmail.com>
2023-08-24 01:56:33 +0300

    replace space to $TAB in FragmentLayoutTemplate.kt

--- src/main/kotlin/ru/redbyte/arch/plugin/data/tmp/FragmentLayoutTemplate.kt
+++ src/main/kotlin/ru/redbyte/arch/plugin/data/tmp/FragmentLayoutTemplate.kt
@@ -1,11 +1,13 @@
 package ru.redbyte.arch.plugin.data.tmp
 
+import ru.redbyte.arch.plugin.data.TAB
+
 class FragmentLayoutTemplate : Template<NoParams> {
     override fun generate(params: NoParams): String {
         return "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
                 "<FrameLayout xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
-                "    android:id=\"@+id/container\"\n" +
-                "    android:layout_width=\"match_parent\"\n" +
-                "    android:layout_height=\"match_parent\" />"
+                "${TAB}android:id=\"@+id/container\"\n" +
+                "${TAB}android:layout_width=\"match_parent\"\n" +
+                "${TAB}android:layout_height=\"match_parent\" />"
     }
 }
