cb83f9f0c0f0dc6054eaf054d92843cf680844bb
Ilya Sokolov <developer.sokolov@gmail.com>
2024-08-21 00:17:06 +0300

    Change params classes with DSL

--- src/main/kotlin/ru/redbyte/arch/plugin/data/templates/Template.kt
+++ src/main/kotlin/ru/redbyte/arch/plugin/data/templates/Template.kt
@@ -3,7 +3,6 @@ package ru.redbyte.arch.plugin.data.templates
 import ru.redbyte.arch.plugin.domain.Feature
 
 interface Template<T : TemplateParams> {
-
     fun generate(params: T): String
 }
 
@@ -11,56 +10,162 @@ sealed interface TemplateParams
 
 object NoParams : TemplateParams
 
-class ManifestParams(
-    val lowerCaseFeatureName: String
+abstract class BaseFeatureParams(
+    val packageName: String,
+    val lowerCaseFeatureName: String,
+    val camelCaseFeatureName: String? = null,
+    val snakeCaseFeatureName: String? = null
 ) : TemplateParams
 
-class BuildGradleParams(val feature: Feature) : TemplateParams {
+class ManifestParams private constructor(
+    packageName: String,
+    lowerCaseFeatureName: String
+) : BaseFeatureParams(packageName, lowerCaseFeatureName) {
+
+    companion object {
+        fun build(init: ManifestParamsBuilder.() -> Unit): ManifestParams {
+            return ManifestParamsBuilder().apply(init).build()
+        }
+    }
+
+    class ManifestParamsBuilder {
+        lateinit var packageName: String
+        lateinit var lowerCaseFeatureName: String
+
+        fun build() = ManifestParams(packageName, lowerCaseFeatureName)
+    }
+}
+
+class BuildGradleParams private constructor(
+    val feature: Feature,
+    packageName: String,
+    lowerCaseFeatureName: String
+) : BaseFeatureParams(packageName, lowerCaseFeatureName) {
 
     val gradleIndent = "    "
 
-    private val defaultApplyFiles: MutableList<String> = mutableListOf(
+    val applyFiles: List<String> = listOf(
         "\"gradle/plugin/android.gradle\"",
         "\"gradle/plugin/kotlin-library.gradle\""
     )
-    private val defaultProjects: MutableList<String> = mutableListOf(
+    val dependenciesProjects: List<String> = listOf(
         "implementation project(':common:lokalise')",
         "implementation project(':common:models')"
     )
-
-    private val defaultLibraries: MutableList<String> = mutableListOf(
+    val dependenciesLibraries: List<String> = listOf(
         "implementation kotlinLibs.kotlinxCoroutinesAndroid",
         "implementation kotlinLibs.kotlinxCoroutinesRuntimeKtx"
     )
+    val annotationProcessors: List<String> = emptyList<String>()
 
-    private val defaultAnnotationProcessors: MutableList<String> = mutableListOf()
+    companion object {
+        fun build(init: BuildGradleParamsBuilder.() -> Unit): BuildGradleParams {
+            return BuildGradleParamsBuilder().apply(init).build()
+        }
+    }
 
-    val applyFiles: List<String> = defaultApplyFiles
-    val dependenciesProjects: List<String> = defaultProjects
-    val dependenciesLibraries: List<String> = defaultLibraries
-    val annotationProcessors: List<String> = defaultAnnotationProcessors
+    class BuildGradleParamsBuilder {
+        lateinit var feature: Feature
+        lateinit var packageName: String
+        lateinit var lowerCaseFeatureName: String
 
+        fun build() = BuildGradleParams(feature, packageName, lowerCaseFeatureName)
+    }
 }
 
-class ScreenParams(
-    val lowerCaseFeatureName: String,
-    val camelCaseFeatureName: String,
-    val snakeCaseFeatureName: String,
+class ScreenParams private constructor(
+    packageName: String,
+    lowerCaseFeatureName: String,
+    camelCaseFeatureName: String,
+    snakeCaseFeatureName: String,
     val createDi: Boolean,
-    val withSetupBackNavigation: Boolean //TODO: extract to setup window
-) : TemplateParams
+    val withSetupBackNavigation: Boolean
+) : BaseFeatureParams(packageName, lowerCaseFeatureName, camelCaseFeatureName, snakeCaseFeatureName) {
 
-class EventParams(
-    val lowerCaseFeatureName: String,
-    val camelCaseFeatureName: String
-) : TemplateParams
+    companion object {
+        fun build(init: ScreenParamsBuilder.() -> Unit): ScreenParams {
+            return ScreenParamsBuilder().apply(init).build()
+        }
+    }
 
-class StateParams(
-    val lowerCaseFeatureName: String,
-    val camelCaseFeatureName: String
-) : TemplateParams
+    class ScreenParamsBuilder {
+        lateinit var packageName: String
+        lateinit var lowerCaseFeatureName: String
+        lateinit var camelCaseFeatureName: String
+        lateinit var snakeCaseFeatureName: String
+        var createDi: Boolean = false
+        var withSetupBackNavigation: Boolean = false
 
-class ReducerParams(
-    val lowerCaseFeatureName: String,
-    val camelCaseFeatureName: String
-) : TemplateParams
+        fun build() = ScreenParams(
+            packageName,
+            lowerCaseFeatureName,
+            camelCaseFeatureName,
+            snakeCaseFeatureName,
+            createDi,
+            withSetupBackNavigation
+        )
+    }
+}
+
+class EventParams private constructor(
+    packageName: String,
+    lowerCaseFeatureName: String,
+    camelCaseFeatureName: String
+) : BaseFeatureParams(packageName, lowerCaseFeatureName, camelCaseFeatureName) {
+
+    companion object {
+        fun build(init: EventParamsBuilder.() -> Unit): EventParams {
+            return EventParamsBuilder().apply(init).build()
+        }
+    }
+
+    class EventParamsBuilder {
+        lateinit var packageName: String
+        lateinit var lowerCaseFeatureName: String
+        lateinit var camelCaseFeatureName: String
+
+        fun build() = EventParams(packageName, lowerCaseFeatureName, camelCaseFeatureName)
+    }
+}
+
+class StateParams private constructor(
+    packageName: String,
+    lowerCaseFeatureName: String,
+    camelCaseFeatureName: String
+) : BaseFeatureParams(packageName, lowerCaseFeatureName, camelCaseFeatureName) {
+
+    companion object {
+        fun build(init: StateParamsBuilder.() -> Unit): StateParams {
+            return StateParamsBuilder().apply(init).build()
+        }
+    }
+
+    class StateParamsBuilder {
+        lateinit var packageName: String
+        lateinit var lowerCaseFeatureName: String
+        lateinit var camelCaseFeatureName: String
+
+        fun build() = StateParams(packageName, lowerCaseFeatureName, camelCaseFeatureName)
+    }
+}
+
+class ReducerParams private constructor(
+    packageName: String,
+    lowerCaseFeatureName: String,
+    camelCaseFeatureName: String
+) : BaseFeatureParams(packageName, lowerCaseFeatureName, camelCaseFeatureName) {
+
+    companion object {
+        fun build(init: ReducerParamsBuilder.() -> Unit): ReducerParams {
+            return ReducerParamsBuilder().apply(init).build()
+        }
+    }
+
+    class ReducerParamsBuilder {
+        lateinit var packageName: String
+        lateinit var lowerCaseFeatureName: String
+        lateinit var camelCaseFeatureName: String
+
+        fun build() = ReducerParams(packageName, lowerCaseFeatureName, camelCaseFeatureName)
+    }
+}
--- src/main/kotlin/ru/redbyte/arch/plugin/data/utils/words.kt
+++ src/main/kotlin/ru/redbyte/arch/plugin/data/utils/words.kt
@@ -1,6 +1,5 @@
 package ru.redbyte.arch.plugin.data.utils
 
-const val PACKAGE_PREFIX = "ru.redbyte.arch" // TODO: 23.08.2023 get value from settings form
 const val INTERNAL = "internal"
 const val CLASS = "class"
 const val DATA_CLASS = "data class"
