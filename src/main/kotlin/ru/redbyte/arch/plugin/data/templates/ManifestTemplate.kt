6e3878415fbb57c045b3d9053a422bea4755240b
Ilya Sokolov <developer.sokolov@gmail.com>
2024-08-21 22:11:29 +0300

    fix make manifest

--- src/main/kotlin/ru/redbyte/arch/plugin/data/templates/ManifestTemplate.kt
+++ src/main/kotlin/ru/redbyte/arch/plugin/data/templates/ManifestTemplate.kt
@@ -3,5 +3,5 @@ package ru.redbyte.arch.plugin.data.templates
 class ManifestTemplate: Template<ManifestParams> {
     override fun generate(params: ManifestParams): String =
         "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                "<manifest package=\"ru.redbyte.arch.${params.lowerCaseFeatureName}\" />"
+                "<manifest package=\"\"${params.packageName}.${params.lowerCaseFeatureName}\" />"
 }
