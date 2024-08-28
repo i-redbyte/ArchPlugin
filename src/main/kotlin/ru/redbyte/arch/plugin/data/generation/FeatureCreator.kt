3aa8b0dfdbb5666de8720539b8278144fd20e4c9
Ilya Sokolov <developer.sokolov@gmail.com>
2024-08-29 01:03:00 +0300

    @fix: move addModuleToSettingsGradle to make module

--- src/main/kotlin/ru/redbyte/arch/plugin/data/generation/FeatureCreator.kt
+++ src/main/kotlin/ru/redbyte/arch/plugin/data/generation/FeatureCreator.kt
@@ -1,14 +1,10 @@
 package ru.redbyte.arch.plugin.data.generation
 
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.psi.PsiDirectory
-import com.intellij.psi.PsiManager
 import ru.redbyte.arch.plugin.domain.Feature
-import java.io.File
-import java.io.IOException
+
 
 class FeatureCreator(val project: Project) {
 
@@ -19,39 +15,8 @@ class FeatureCreator(val project: Project) {
         ApplicationManager.getApplication().runWriteAction {
             with(targetDirectory) {
                 MakeModule(feature).createModuleStructure(this)
-                addModuleToSettingsGradle(feature, this)
             }
         }
     }
 
-    private fun addModuleToSettingsGradle(feature: Feature, directory: PsiDirectory) {
-        val basePath = project.basePath
-        val baseDir = basePath?.let { LocalFileSystem.getInstance().findFileByPath(it) }
-        val settingsFile = baseDir?.findChild("settings.gradle.kts") ?: baseDir?.findChild("settings.gradle")
-
-        settingsFile?.let {
-            val psiFile = PsiManager.getInstance(project).findFile(it)
-            val relativePath = directory.virtualFile.path
-                .removePrefix(basePath.toString())
-                .replace(File.separator, ":")
-                .removePrefix(":")
-            val moduleName = ":$relativePath:${feature.params.featureName.lowercase()}"
-
-            psiFile?.let { file ->
-                val content = file.text
-                if (!content.contains(moduleName)) {
-                    WriteCommandAction.runWriteCommandAction(project) {
-                        try {
-                            val document = file.viewProvider.document
-                            document?.let { doc ->
-                                doc.insertString(doc.textLength, "\ninclude '$moduleName'")
-                            }
-                        } catch (e: IOException) {
-                            e.printStackTrace()
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
--- src/main/kotlin/ru/redbyte/arch/plugin/data/generation/MakeModule.kt
+++ src/main/kotlin/ru/redbyte/arch/plugin/data/generation/MakeModule.kt
@@ -1,9 +1,14 @@
 package ru.redbyte.arch.plugin.data.generation
 
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiManager
 import ru.redbyte.arch.plugin.data.templates.*
 import ru.redbyte.arch.plugin.data.utils.NamesBuilder
 import ru.redbyte.arch.plugin.domain.Feature
+import java.io.File
+import java.io.IOException
 
 class MakeModule(private val feature: Feature) : Module() {
 
@@ -28,6 +33,7 @@ class MakeModule(private val feature: Feature) : Module() {
             makeDIPackage(withDIFiles)
             makeResValuesPackage()
         }
+        addModuleToSettingsGradle(directory)
     }
 
     private fun makeDIPackage(withDIFiles: Boolean) {
@@ -117,5 +123,36 @@ class MakeModule(private val feature: Feature) : Module() {
         }
     }
 
+    private fun addModuleToSettingsGradle(directory: PsiDirectory) {
+        val project = directory.project
+        val basePath = project.basePath
+        val baseDir = basePath?.let { LocalFileSystem.getInstance().findFileByPath(it) }
+        val settingsFile = baseDir?.findChild("settings.gradle.kts") ?: baseDir?.findChild("settings.gradle")
+
+        settingsFile?.let {
+            val psiFile = PsiManager.getInstance(project).findFile(it)
+            val relativePath = directory.virtualFile.path
+                .removePrefix(basePath.toString())
+                .replace(File.separator, ":")
+                .removePrefix(":")
+            val moduleName = ":$relativePath:${feature.params.featureName.lowercase()}"
+
+            psiFile?.let { file ->
+                val content = file.text
+                if (!content.contains(moduleName)) {
+                    WriteCommandAction.runWriteCommandAction(project) {
+                        try {
+                            val document = file.viewProvider.document
+                            document?.let { doc ->
+                                doc.insertString(doc.textLength, "\ninclude '$moduleName'")
+                            }
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 }
